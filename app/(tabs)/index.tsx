4a8c94b2194ca9c2b183cccd79c161c1eae4f7f9
lutkenhaus <gustavomayron9@gmail.com>
2024-05-15 18:23:53 -0300

    blank project

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,6 +5,7 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
+// comentário teste
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
